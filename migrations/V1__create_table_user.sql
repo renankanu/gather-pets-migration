b0aa708208a2294b7313c3822d3c148ff38f24cc
RenanKanu <renankanu@gmail.com>
2020-06-05 16:49:58 -0300

    Refactor migration V1

--- migrations/V1__create_table_user.sql
+++ migrations/V1__create_table_user.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `gather_pets_db`.`user` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` VARCHAR(36) NOT NULL,
   `name` VARCHAR(155) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(60) NOT NULL,
   `createdAt` DATETIME NOT NULL,
   `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
